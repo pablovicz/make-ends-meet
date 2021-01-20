3b1a02b2c8b06d8c1b332b39f5f1d5ae34c5ae72
Pablo <pablowoina2205@gmail.com>
2021-01-20 08:39:19 -0300

    Service Layer Initial Commit

---
+++ mem-service/src/main/java/com/pablovicz/memservice/MakeEndsMeetApplication.java
@@ -0,0 +1,13 @@
+package com.pablovicz.memservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeEndsMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeEndsMeetApplication.class, args);
+	}
+
+}
---
+++ mem-service/src/test/java/com/pablovicz/memservice/MakeEndsMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablovicz.memservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeEndsMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
